6e18cc7351e628ce76b52634463ca0a6dab3fd6b
sdmm5 <sdmm5at@gmail.com>
2016-12-13 01:54:46 +0800

    Initial Commit

---
+++ Logxz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Logxz
+//
+//  Created by lip on 16/12/13.
+//  Copyright © 2016年 com.sdmm5at. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
